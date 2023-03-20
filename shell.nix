a0abe0d1f49ba807a7a61dba7f812dc117e010d2
卍diohabara卍 <diohabara@gmail.com>
2023-03-20 01:18:15 -0500

    wip

--- shell.nix
+++ shell.nix
@@ -2,8 +2,9 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "llvm-dev";
   buildInputs = [
+    gnumake
     llvmPackages_15.libcxxClang
-    # llvmPackages_15.clangUseLLVM
+    llvmPackages_15.libllvm
     llvmPackages_15.llvm
   ];
 }
